84e7db60152ce17d2f2eb72459f3ee6700f18c84
Angielnani <surzhikovy@mail.ru>
2021-07-23 09:25:07 +0300

    task done

---
+++ task.rb
@@ -0,0 +1,17 @@
+class String
+  def row
+    package = []
+    self.each_char do |array| #строковый итератор
+      if package.last && package.last.last == array 
+        package.last << array
+      else
+        package << [array]
+      end
+    end
+
+    package.map { |i| [i.size, i.last] }.join
+  end
+end
+
+puts res = '1'
+6.times { puts res = res.row } #ещё один класный строковый итератор
